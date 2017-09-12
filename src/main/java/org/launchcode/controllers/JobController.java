541f56590369695f3d49ce189591bbf4d757ac26
ctam91 <ctam91@gmail.com>
2017-09-11 20:00:59 -0700

    implement case sensitivity

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -47,7 +47,7 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
         if(errors.hasErrors()){
-            return "job/add";
+            return "new-job";
         }
 
         int employerId = jobForm.getEmployerId();
